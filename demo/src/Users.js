8fdaca3661ed8f6c23f730fddf83ffd597a75960
Sid Ferreira <sid@ferreiraz.com.br>
2017-07-14 12:49:00 -0300

    Improves DEMO version

---
+++ demo/src/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { List, Datagrid, EmailField, TextField } from 'admin-on-rest';
+
+export const UserList = (props) => (
+    <List title="All users" {...props}>
+        <Datagrid>
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+        </Datagrid>
+    </List>
+);
